e5c7155557247d20b925378f9aa0265a077b5713
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-07-20 00:35:45 +0530

    add ppr results

--- personalised-pagerank/ppr.py
+++ personalised-pagerank/ppr.py
@@ -2,7 +2,7 @@ import pandas as pd
 from collections import defaultdict
 import json
 
-# Load transaction graph data from tsv file
+# Load transaction graph data from data files
 graph_path = "./pp-data/txedges.dat"
 df = pd.read_csv(graph_path, sep=r"\s+", names=['txid', 'inaddr', 'outaddr', 'weight'])
 
@@ -12,7 +12,7 @@ fraud_nodes = list(df_scam['addr'])
 
 # Data Pre-processing
 G = defaultdict(list)       # G represents graph
-for row in df.head().itertuples():
+for row in df.itertuples():
     G[row.inaddr].append(row.outaddr)   #directed edge from inaddr to outaddr
     G[row.outaddr]                      #to ensure dangling nodes (out-degree=0) have an empty list
 
@@ -33,7 +33,7 @@ for node in G.keys():
 
 alpha = 0.85 # damping parameter
 max_iter = 1000 # chosen constant
-tol = float(10e-12)
+eps = float(10e-12)
 
 y = dict.fromkeys(G, 1/n) #starting vector for PageRank score
 
@@ -45,17 +45,16 @@ for run in range(max_iter):
     for i in G:
         s = sum(y[j]/outdeg(j) for j in G.keys() if (i in G[j]))
         y_i_new = v[i] + alpha*s
-        # Computes the L1 norm without explicitly memorize the previous iteration
         err += abs(y_i_new - y[i])
         y[i] = y_i_new
 
-    if (err < tol):
+    if (err < eps):
         norm = sum(y.values())
         z = dict((k, v / norm) for k, v in y.items())
         break
 
 # Check that PPR vector converged 
-if (err > tol):
+if (err > eps):
     raise Exception(f"Personalised PageRank did not converge in {max_iter} iterations.")
 
 # Save PPR scores in descending order
